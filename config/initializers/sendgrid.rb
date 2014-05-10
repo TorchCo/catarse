99740e65b4627b0d2b52a01dc1593e648ee0de61
Afolabi Badmos <afolabi@badmos.com>
2014-05-10 16:31:29 -0400

    Pulls sendgrid creds from ENV instead of from DB

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,11 +1,11 @@
 begin
-  if Rails.env.production?
+  if Rails.env.staging? || Rails.env.production?
     ActionMailer::Base.smtp_settings = {
       address: 'smtp.sendgrid.net',
       port: '587',
       authentication: :plain,
-      user_name: CatarseSettings[:sendgrid_user_name],
-      password: CatarseSettings[:sendgrid],
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
       domain: 'heroku.com'
     }
     ActionMailer::Base.delivery_method = :smtp
