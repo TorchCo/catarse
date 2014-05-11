1a81260585ead27a8813ba0c85bb6ab78beeac6c
Afolabi Badmos <afolabi@badmos.com>
2014-05-11 18:06:20 -0400

    Get AWS creds from ENV instead of DB

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-  if Rails.env.production? and CatarseSettings[:aws_access_key]
+  if Rails.env.staging? || Rails.env.production?
     config.fog_credentials = {
       provider: 'AWS',
       host: 's3.amazonaws.com',
       endpoint: 'http://s3.amazonaws.com',
-      aws_access_key_id: CatarseSettings[:aws_access_key],
-      aws_secret_access_key: CatarseSettings[:aws_secret_key]
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY']
     }
-    config.fog_directory  = CatarseSettings[:aws_bucket]
+    config.fog_directory  = ENV['AWS_BUCKET']
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   else
     config.enable_processing = false if Rails.env.test? or Rails.env.cucumber?
