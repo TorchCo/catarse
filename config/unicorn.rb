e0b33add4472d5d60907254335608fa87860d2d7
Afolabi Badmos <afolabi@badmos.com>
2014-05-11 11:19:07 -0400

    Trying to use heroku webworker to run sidekiq

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,7 +12,6 @@ preload_app true
 
 
 before_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -20,16 +19,25 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
+
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 end
 
 after_fork do |server, worker|
-
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
   end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
+
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
 end
 
 
